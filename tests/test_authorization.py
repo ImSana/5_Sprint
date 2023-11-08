1d82e5a2ce5d797c5a40ff54f4dd5171bbc0ff28
ImSana <aasursyakov@gmail.com>
2023-11-08 22:38:24 +0300

    final fix 2

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support import expected_conditions as ex_cond
 from locators import TestLocators
 from selenium.webdriver.support.ui import WebDriverWait
+import data
 class TestAuthorization:
 
     def test_go_account(self, driver_settings_chrome):
@@ -8,10 +9,8 @@ class TestAuthorization:
         driver_settings_chrome.find_element(*TestLocators.ENTER_IN_ACCOUNT_BUTTON).click()
         WebDriverWait(driver_settings_chrome, 5).until(ex_cond.url_contains("/login"))
 
-        email = "uncle_Bens111@ya.ru"
-        password = "123123"
-        driver_settings_chrome.find_element(*TestLocators.EMAIL_FIELD).send_keys(email)
-        driver_settings_chrome.find_element(*TestLocators.PASSWORD_FIELD).send_keys(password)
+        driver_settings_chrome.find_element(*TestLocators.EMAIL_FIELD).send_keys(data.email)
+        driver_settings_chrome.find_element(*TestLocators.PASSWORD_FIELD).send_keys(data.password)
 
         WebDriverWait(driver_settings_chrome, 5).until(ex_cond.element_to_be_clickable(TestLocators.ENTER_BUTTON))
         driver_settings_chrome.find_element(*TestLocators.ENTER_BUTTON).click()
@@ -19,7 +18,6 @@ class TestAuthorization:
 
         driver_settings_chrome.find_element(*TestLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver_settings_chrome, 5).until(ex_cond.url_contains("/account/profile"))
-
 
         driver_settings_chrome.find_element(*TestLocators.ACCOUNT_MENU)
         assert "Bens" == driver_settings_chrome.find_element(*TestLocators.NAME_FIELD).get_attribute("value")
